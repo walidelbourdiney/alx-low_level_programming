d99fc47702561339e67b8e240d56498abb2aaf9a
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-02-21 19:01:10 -0800

    Typo

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,12 @@
  */
 void print_alphabet(void)
 {
-	char letter = 'a';
+	char c = 'a';
 
-	while (letter <= 'z')
+	while (c <= 'z')
 	{
-		_putchar(letter);
-		letter++;
+		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
