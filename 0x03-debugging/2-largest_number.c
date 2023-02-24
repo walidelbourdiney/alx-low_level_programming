000f626b43a211b9b1779b8790095018075de092
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-02-23 16:01:30 -0800

    4th

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,17 +9,19 @@
  */
 int largest_number(int a, int b, int c)
 {
-	int largest = b;
+	int largest;
 
-	if (a > b)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (c > b)
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
-
