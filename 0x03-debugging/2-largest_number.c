9128ec13f41c1ca08a827558e8e358980fe27863
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-02-23 16:12:43 -0800

    TRY

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,17 +11,21 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b >= a && b >= c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > a && c > b)
 	{
 		largest = c;
+	}
+	else
+	{
+		printf("check your inputs!");
 	}
 	return (largest);
 }
