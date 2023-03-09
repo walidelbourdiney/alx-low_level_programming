6a2b3643b249b8ddc9ccba945e185dca6b3d3166
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-03-09 11:30:23 -0800

    task 8

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * Description: Returns the factorial of a given number
+ * @n: Integer
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
