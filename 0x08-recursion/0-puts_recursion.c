f76414d2f76de77c4195c065508c0779cb2c0548
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-11-15 06:21:16 -0800

    betty 3

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 	#include "main.h"
 
 	/**
- 	* _puts_recursion - Entry point
- 	* Description: Prints a string, followed by a new line using recursion
- 	* @s: Character
- 	*/
+	* _puts_recursion - Entry point
+	* _puts_recursion Description: Prints a string, followed by a new line using recursion
+	* @s: Character
+	*/
 
 	void _puts_recursion(char *s)
 	{
