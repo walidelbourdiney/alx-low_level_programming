bf5457ce3d142fef0b0d969bcc87a5df8ae2490b
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-11-27 13:59:42 -0800

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
