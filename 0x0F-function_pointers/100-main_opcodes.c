f907213d450359bd904ec392e95ec7d2f1b286b2
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-11-28 23:14:19 -0800

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_opcodes(char *buffer, int num_bytes);
+/**
+ * main - entry point to the program
+ * @argc: num of arguments
+ * @argv: array of char
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	char *ptr = (char *)main;
+	int n;
+
+	if (argc != 2)
+	{
+		printf("ERROR\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("ERROR\n");
+		exit(2);
+	}
+	while (n--)
+	{
+		printf("%02hhx%s", *ptr++, n ? " " : "\n");
+	}
+	return (0);
+}
