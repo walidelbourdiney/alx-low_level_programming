a172126d5524f91316e7fa5e16f00e5c02ea40ea
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-11-29 11:38:25 -0800

    04 last

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_opcodes(char *buffer, int num_bytes);
 /**
  * main - entry point to the program
  * @argc: num of arguments
  * @argv: array of char
- * Return: 0 always
+ *
+ * Return: 0 always wins
  */
 int main(int argc, char *argv[])
 {
-	int num_butes, i;
-	char *address;
+	char *ptr = (char *)main;
+	int n;
 
 	if (argc != 2)
 	{
-		printf("ERROR\n");
-		return (1);
+		printf("Error\n");
+		exit(1);
 	}
-
-	num_bytes = atoi(argv[1]);
-
-	if (num_bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
-		printf("ERROR\n");
-		return (2);
+		printf("Error\n");
+		exit(2);
 	}
-	address = (char *)main;
-	for (i = 0; i < num_bytes; i++)
+	while (n--)
 	{
-		printf("%02hhx%c", address[i], i < num_bytes - 1 ? ' ' : '\n');
+		printf("%02hhx%s", *ptr++, n ? " " : "\n");
 	}
 	return (0);
 }
