3ab6e70a3d6b4c7c7f9b5fa84783193dc170a4b2
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-11-28 22:46:36 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: name of a person
+ * @f: finction pointer
+ * Return: noting or void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
