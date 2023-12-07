7ab8b1b09cfa55c9951b21114e85f5ba70def450
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-12-07 13:37:23 -0800

    modify task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		cnt++;
 		h = h->next;
 	}
