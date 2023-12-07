837cc467bc3da2576775415656e731d4d4858183
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-12-07 13:33:29 -0800

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print list - function that prints all the elements of list_t list
+ * @h: struct node
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t cnt = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->str);
+		cnt++;
+		h = h->next;
+	}
+
+	return (cnt);
+}
