eca10621e0cd944941e35a441c852b3b22ce14c9
walidelbourdiney <walidelbourdiney25@gmail.com>
2023-12-12 15:27:36 -0800

    redo task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		/* Checks if current node and next node is null before inserting */
 		if (current == NULL || current->next == NULL)
 		{
-			return (NULL)
+			return (NULL);
 		}
 		current  = current->next;
 	}
