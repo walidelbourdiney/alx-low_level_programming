678734aa4303bd931644b4ce0dcae0c3d06f2935
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-03-22 04:20:12 -0700

    conf

---
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Author: Promise Nwhator
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+start = int(input("Enter the starting value (3-digit number): "))
+end = int(input("Enter the ending value (3-digit number): "))
+
+
+largest_palindrome = 0
+
+for i in range(start, end + 1):
+    for j in range(i, end + 1):
+        product = i * j
+        if is_palindrome(product) and product > largest_palindrome:
+            largest_palindrome = product
+                                            
+
+with open('102-result', 'w') as file:
+    file.write(str(largest_palindrome))
